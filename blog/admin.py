f3efe7c965f73d6436771b126d2f413c647423c2
ken <626014896@qq.com>
2018-04-28 13:10:46 +0800

    url and http

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Tag,Post,Category
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ("title","body","created_time","modified_time","excerpt","category","author",)
+
+
+
+
+admin.site.register(Tag,TagAdmin)
+
+admin.site.register(Category,CategoryAdmin)
+
+admin.site.register(Post,PostAdmin)
