d7333ddd79f8376c1d3a28deb84103918be4f5c5
ken <626014896@qq.com>
2018-04-28 15:32:20 +0800

    updata homeage

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from blog.models import Post
 
 
 # Create your views here.
 def  index(requset):
+    articles=Post.objects.all()
+    return  render(requset,"blog/index.html",{"articles":articles
 
-    return  render(requset,"blog/index.html",{"titile":"my homepage",
-                                              "welcome":"welcome my page"
 
                                               })
